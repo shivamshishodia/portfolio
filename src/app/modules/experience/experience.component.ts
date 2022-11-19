a299663e50a6bfc4c5a3a0d286afb92c5d525a67
Shivam Shishodia <theshishodia@gmail.com>
2022-11-20 01:12:38 +0530

    Added references of Oracle

--- src/app/modules/experience/experience.component.ts
+++ src/app/modules/experience/experience.component.ts
@@ -17,9 +17,16 @@ export class ExperienceComponent implements OnInit {
 
   experiences: Experience[] = [
     {
+      position: "Software Developer 2",
+      company: "Oracle",
+      experience: "Oct. 2022 - Present",
+      organisation: "Logging Analytics (OCI)",
+      breif: "Working on on-demand collectors for management agent; shipped to over 700+ customers. Agents are designed to enrich, preprocess, and cumulatively handle over 40 terabytes of data per day."
+    },
+    {
       position: "Senior Software Engineer",
       company: "Dell",
-      experience: "Apr. 2022 - Present",
+      experience: "Apr. 2022 - Oct. 2022",
       organisation: "Demand Planning (MTRC)",
       breif: "Developed real-time Spring boot-based microservices to process the high volume of sales records (8k/hour). Improved robustness by handling points of failure. Increased data accuracy to 98 percent."
     },
@@ -43,6 +50,13 @@ export class ExperienceComponent implements OnInit {
       experience: "Jan. 2018 - May 2018",
       organisation: "DCE Content Operations",
       breif: "Designed and developed EOL Audit Automation capable of saving 5,992 man-hours per year. The codebase was deployed as a core application and saved 5,880 man-hours per year processing EOL requests."
+    },
+    {
+      position: "Chapter Officer",
+      company: "ACM",
+      experience: "Aug. 2016 - Aug. 2017",
+      organisation: "Web Development Lead",
+      breif: "We lead over 250 university students, training them for competitive programming and industry-level best practices. I served as the office bearer for over 1 year 2 months."
     }
   ]
 
